1b1eba6e9d3eb6578fe19f19b8ac6095767f55de
unknown <s.dalov98@gmail.com>
2024-03-17 10:13:24 +0200

    First commit

---
+++ ASP-net-Crud-With-UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_net_Crud_With_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
